75d9a1bac3508b83f695f484126b953f254f3ea4
osdever <10074604+osdeverr@users.noreply.github.com>
2022-11-26 21:49:15 +0300

    [all] Implement performance profiling

---
+++ rebuildkit/re/debug.cpp
@@ -0,0 +1,39 @@
+#include "debug.h"
+
+#include <fmt/color.h>
+
+namespace re
+{
+	void PerfProfile::Finish()
+	{
+		mEndTime = std::chrono::high_resolution_clock::now();
+		mFinished = true;
+	}
+
+	void PerfProfile::Print()
+	{
+		const auto style = fmt::emphasis::bold | fg(fmt::color::green_yellow);
+
+		fmt::print(
+			style,
+			"\n *** [PERF] [{}]\n", mName
+		);
+
+		fmt::print(
+			style,
+			"    {} ", mFinished ? "finished in" : "running for"
+		);
+
+		auto point = mFinished ? mEndTime : std::chrono::high_resolution_clock::now();
+		auto delta = point - mBeginTime;
+
+		auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(delta).count();
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(delta).count();
+		auto s = ms / 1000.f;
+
+		fmt::print(
+			style, 
+			"{}s / {}ms / {}ns\n", s, ms, ns
+		);
+	}
+}
