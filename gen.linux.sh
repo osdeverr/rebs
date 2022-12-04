86729378f92d4bd98af18d72db4917927b7a41c3
osdever <krasota156@gmail.com>
2022-12-04 17:15:57 +0300

    [all] Move the project to Re

--- gen.linux.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-mkdir -p out
-cd out
-cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake \
-    -DCMAKE_INSTALL_PREFIX="/usr/local/" \
-    -DCMAKE_BUILD_TYPE="RelWithDebInfo"
--- re/root/re-tool-env.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set RE_PLATFORM=windows
-set RE_ARCH=%3
-
-call %0\..\data\env-scripts\%2.cmd %*
