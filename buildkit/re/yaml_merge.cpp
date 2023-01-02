5cd5a592eb4764772e236c9991f2580df675e2be
osdever <krasota156@gmail.com>
2023-01-02 19:03:14 +0300

    [buildkit] Fix target config merging

--- buildkit/re/yaml_merge.cpp
+++ buildkit/re/yaml_merge.cpp
@@ -1,11 +1,13 @@
 #include "yaml_merge.h"
 
 #include <re/error.h>
+#include <re/debug.h>
+
 #include <stdexcept>
 
 namespace re
 {
-	void MergeYamlNode(YAML::Node target, const YAML::Node& source, bool overridden)
+	void MergeYamlNode(YAML::Node& target, const YAML::Node& source, bool overridden)
 	{
 		switch (source.Type())
 		{
@@ -42,9 +44,15 @@ namespace re
 			constexpr auto kOverridePrefix = "override.";
 
 			if (overridden || key.find(kOverridePrefix) == 0)
+			{
+				RE_TRACE("OVERRIDE PREFIX found on {}\n", key);
 				MergeYamlNode(target[key.substr(sizeof kOverridePrefix + 1)], j.second, true);
+			}
 			else
+			{
+				RE_TRACE("Not overriding {}\n", key);
 				MergeYamlNode(target[key], j.second);
+			}
 		}
 	}
 
@@ -52,11 +60,16 @@ namespace re
 	{
 		if (overridden)
 		{
-			target = Clone(source);
+			RE_TRACE("{} - override enabled, will copy over\n", target.Scalar());
+
+			target = YAML::Clone(source);
 			return;
 		}
+		else
+			RE_TRACE("{} - override disabled\n", target.Scalar());
 
 		for (std::size_t i = 0; i != source.size(); ++i) {
+			RE_TRACE("  Adding {}\n", source[i].Scalar());
 			target.push_back(YAML::Clone(source[i]));
 		}
 	}
