834a349cfd02710b90a02c11a07c1416f4984534
osdever <krasota156@gmail.com>
2023-01-03 16:03:04 +0300

    [buildkit] Migrate to Boost.Process in process_util

--- buildkit/re/process_util.h
+++ buildkit/re/process_util.h
@@ -10,7 +10,7 @@ namespace re
 {
     int RunProcessOrThrow(
         std::string_view program_name,
-        const std::vector<std::string>& cmdline,
+        std::vector<std::string> cmdline,
         bool output = false,
         bool throw_on_bad_exit = false,
         std::optional<std::string_view> working_directory = std::nullopt
@@ -19,7 +19,7 @@ namespace re
 #ifdef WIN32
     int RunProcessOrThrowWindows(
         std::string_view program_name,
-        const std::vector<std::wstring>& cmdline,
+        std::vector<std::wstring> cmdline,
         bool output = false,
         bool throw_on_bad_exit = false,
         std::optional<std::wstring_view> working_directory = std::nullopt
