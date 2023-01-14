28474e320c9c1f156f8e396364a4c4ee4ea90351
osdever <krasota156@gmail.com>
2023-01-14 15:36:01 +0300

    [docs] Document re/builenv.h (WIP)

--- buildkit/re/buildenv.h
+++ buildkit/re/buildenv.h
@@ -1,3 +1,13 @@
+/**
+ * @file re/buildenv.h
+ * @author Nikita Ivanov
+ * @brief Low-level target resolution and build functionality
+ * @version 0.2.8
+ * @date 2023-01-14
+ * 
+ * @copyright Copyright (c) 2023 Nikita Ivanov
+ */
+
 #pragma once
 #include "target.h"
 #include "build_desc.h"
@@ -17,27 +27,89 @@
 
 namespace re
 {
+	/**
+	 * @brief A function capable of resolving target dependencies.
+	 */
 	using TargetDepResolver = std::function<bool (const Target&, const TargetDependency&, std::vector<Target*>&)>;
 
+	/**
+	 * @brief Populates the specified collection with a flat target dependency set, resolving all target dependences in progress.
+	 * 
+	 * @param pTarget The root target to start with
+	 * @param to The destination collection
+	 * @param dep_resolver The dependency resolver to use
+	 * @param throw_on_missing Throw exceptions if any dependency is missing
+	 */
 	void PopulateTargetDependencySet(Target* pTarget, std::vector<Target*>& to, TargetDepResolver dep_resolver = {}, bool throw_on_missing = true);
+
+	/**
+	 * @brief Populates the specified collection with a flat target dependency set without resolving any target dependencies.
+	 * 
+	 * @param pTarget The root target to start with
+	 * @param to The destination collection
+	 */
 	void PopulateTargetDependencySetNoResolve(const Target* pTarget, std::vector<const Target*>& to);
 
+	/**
+	 * @brief The "build environment" responsible for loading and resolving targets.
+	 */
 	class BuildEnv : public ILangLocator, public ITargetLoader
 	{
 	public:
+		/**
+		 * @brief Construct a new BuildEnv object from a var scope and an output interface.
+		 * 
+		 * @param scope The root variable scope
+		 * @param pOut The output interface to use
+		 */
 		BuildEnv(LocalVarScope& scope, IUserOutput* pOut);
 
+		/**
+		 * @brief Loads _the_ core project target considered the root target of all other targets.
+		 * 
+		 * @param path The core project target's path
+		 * @return Target& A reference to the core project target
+		 */
 		Target& LoadCoreProjectTarget(const fs::path& path);
 
+		/**
+		 * @brief Gets the core project target.
+		 * 
+		 * @return Target* A pointer to the core project target
+		 */
 		Target* GetCoreTarget();
 
+		/**
+		 * @brief Loads a "free" target without registering it anywhere.
+		 * 
+		 * It is up to the caller to further register the target and be responsible about its ownership.
+		 * This method will use ITargetMiddleware to try loading the target in absense of a `re.yml`.
+		 * 
+		 * @param path The target path
+		 * @param ancestor The target's "ancestor" target responsible for it (can be nullptr)
+		 * @param dep_source The target's "source" dependency which caused it to be loaded (can be nullptr)
+		 * 
+		 * @return std::unique_ptr<Target> A unique pointer to the loaded target.
+		 */
 		std::unique_ptr<Target> LoadFreeTarget(
 			const fs::path& path,
 			const Target* ancestor = nullptr,
 			const TargetDependency* dep_source = nullptr
 		);
 		
+		/**
+		 * @brief Loads a root-level target.
+		 * 
+		 * @param path The target path
+		 * @return Target& A reference to the loaded target.
+		 */
 		Target& LoadTarget(const fs::path& path);
+
+		/**
+		 * @brief Registers the specified target to be available as a local dependency.
+		 * 
+		 * @param pTarget The target to register
+		 */
 		void RegisterLocalTarget(Target* pTarget);
 
 		std::vector<Target*> GetSingleTargetDepSet(Target* pTarget);
