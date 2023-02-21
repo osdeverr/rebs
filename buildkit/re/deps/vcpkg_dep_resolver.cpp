bf265b777b82f80e42b64506e39edcb19ec352f0
osdever <krasota156@gmail.com>
2023-02-21 15:53:52 +0300

    [buildkit] VcpkgDepResolver: Format source file

--- buildkit/re/deps/vcpkg_dep_resolver.cpp
+++ buildkit/re/deps/vcpkg_dep_resolver.cpp
@@ -1,7 +1,7 @@
 #include "vcpkg_dep_resolver.h"
 
-#include <fmt/format.h>
 #include <fmt/color.h>
+#include <fmt/format.h>
 
 #include <re/process_util.h>
 #include <re/target_cfg_utils.h>
@@ -11,56 +11,47 @@
 
 namespace re
 {
-	Target* VcpkgDepResolver::ResolveTargetDependency(const Target& target, const TargetDependency& dep, DepsVersionCache* cache)
-	{
+    Target *VcpkgDepResolver::ResolveTargetDependency(const Target &target, const TargetDependency &dep,
+                                                      DepsVersionCache *cache)
+    {
         auto [scope, context] = target.GetBuildVarScope();
 
         auto re_arch = scope.ResolveLocal("arch");
         auto re_platform = scope.ResolveLocal("platform");
         auto re_config = scope.ResolveLocal("configuration");
 
-        auto cache_path = fmt::format("{}-{}-{}-{}", dep.name, re_arch, re_platform, re_config);
+        auto at_prefix = (dep.version.size()) ? fmt::format("-{}", dep.version) : "";
 
-		if (dep.extra_config_hash)
-			cache_path += fmt::format("-ecfg-{}", dep.extra_config_hash);
+        auto cache_path = fmt::format("{}{}-{}-{}-{}", dep.name, at_prefix, re_arch, re_platform, re_config);
 
-        if (auto& cached = mTargetCache[cache_path])
+        if (dep.extra_config_hash)
+            cache_path += fmt::format("-ecfg-{}", dep.extra_config_hash);
+
+        if (auto &cached = mTargetCache[cache_path])
             return cached.get();
 
         auto vcpkg_root = mVcpkgPath;
 
         if (auto path = scope.GetVar("vcpkg-root-path"))
-            vcpkg_root = fs::path{ *path };
+            vcpkg_root = fs::path{*path};
 
         auto dep_str = dep.ToString();
 
         if (!fs::exists(vcpkg_root / ".git"))
         {
             if (scope.ResolveLocal("auto-load-uncached-deps") != "true")
-                RE_THROW TargetUncachedDependencyException(&target, "Cannot auto-download vcpkg - autoloading is disabled", dep_str);
+                RE_THROW TargetUncachedDependencyException(
+                    &target, "Cannot auto-download vcpkg - autoloading is disabled", dep_str);
 
-            mOut->Info(
-                fmt::emphasis::bold | fg(fmt::color::light_sea_green),
-                "[{}] [vcpkg] Target dependency '{}' needs vcpkg. Installing...\n\n",
-                target.module,
-                dep_str
-            );
+            mOut->Info(fmt::emphasis::bold | fg(fmt::color::light_sea_green),
+                       "[{}] [vcpkg] Target dependency '{}' needs vcpkg. Installing...\n\n", target.module, dep_str);
 
             fs::create_directories(vcpkg_root);
 
             // Try cloning it to Git.
-            RunProcessOrThrow(
-                "git",
-                {},
-                {
-                    "git",
-                    "clone",
-                    "https://github.com/microsoft/vcpkg.git",
-                    vcpkg_root.u8string()
-                },
-                true,
-                true
-            );
+            RunProcessOrThrow("git", {},
+                              {"git", "clone", "https://github.com/microsoft/vcpkg.git", vcpkg_root.u8string()}, true,
+                              true);
 
 #ifdef WIN32
             std::system((vcpkg_root / "bootstrap-vcpkg").string().c_str());
@@ -78,20 +69,17 @@ namespace re
             */
         }
 
-        auto path = vcpkg_root / "packages" / (dep.name + fmt::format("_{}-{}", re_arch, re_platform));
+        auto path = vcpkg_root / "packages" / (dep.name + fmt::format("_{}-{}{}", re_arch, re_platform, at_prefix));
 
         // We optimize the lookup by not invoking vcpkg at all if the package is already there.
         if (!fs::exists(path / "BUILD_INFO"))
         {
             if (scope.ResolveLocal("auto-load-uncached-deps") != "true")
-                RE_THROW TargetUncachedDependencyException(&target, "Cannot resolve uncached dependency {} - autoloading is disabled", dep_str);
+                RE_THROW TargetUncachedDependencyException(
+                    &target, "Cannot resolve uncached dependency {} - autoloading is disabled", dep_str);
 
-            fmt::print(
-                fmt::emphasis::bold | fg(fmt::color::light_green),
-                "[{}] Restoring package {}...\n\n",
-                target.module,
-                dep_str
-            );
+            fmt::print(fmt::emphasis::bold | fg(fmt::color::light_green), "[{}] Restoring package {}...\n\n",
+                       target.module, dep_str);
 
             auto start_time = std::chrono::high_resolution_clock::now();
 
@@ -100,15 +88,9 @@ namespace re
             if (re_platform == "windows")
                 vcpkg_name += ".exe";
 
-            RunProcessOrThrow(
-                "vcpkg",
-                vcpkg_root / vcpkg_name,
-                {
-                    "install",
-                    fmt::format("{}:{}-{}", dep.name, re_arch, re_platform)
-                },
-                true, true
-            );
+            RunProcessOrThrow("vcpkg", vcpkg_root / vcpkg_name,
+                              {"install", fmt::format("{}:{}-{}{}", dep.name, re_arch, re_platform, at_prefix)}, true,
+                              true);
 
             // Throw if the package still isn't there
             if (!fs::exists(path))
@@ -118,25 +100,17 @@ namespace re
 
             auto end_time = std::chrono::high_resolution_clock::now();
 
-            mOut->Info(
-                fmt::emphasis::bold | fg(fmt::color::light_green),
-                "\n[{}] Restored package {} ({:.2f}s)\n",
-                target.module,
-                dep_str,
-                std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() / 1000.f
-            );
+            mOut->Info(fmt::emphasis::bold | fg(fmt::color::light_green), "\n[{}] Restored package {} ({:.2f}s)\n",
+                       target.module, dep_str,
+                       std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() / 1000.f);
         }
         else
         {
-            mOut->Info(
-                fmt::emphasis::bold | fg(dep.ns == "vcpkg-dep" ? fmt::color::dim_gray : fmt::color::light_green),
-                "[{}] Package {} already available\n",
-                target.module,
-                dep_str
-            );
+            mOut->Info(fmt::emphasis::bold | fg(dep.ns == "vcpkg-dep" ? fmt::color::dim_gray : fmt::color::light_green),
+                       "[{}] Package {} already available\n", target.module, dep_str);
         }
 
-        YAML::Node config{ YAML::NodeType::Map };
+        YAML::Node config{YAML::NodeType::Map};
 
         if (fs::exists(path / "include"))
         {
@@ -149,7 +123,7 @@ namespace re
 
         if (fs::exists(path / "lib"))
         {
-            for (auto& file : fs::directory_iterator{ path / "lib" })
+            for (auto &file : fs::directory_iterator{path / "lib"})
             {
                 if (file.is_regular_file() && file.path().extension() != ".pdb")
                     config["cxx-link-deps"].push_back(file.path().u8string());
@@ -158,7 +132,7 @@ namespace re
 
         if (fs::exists(path / "bin"))
         {
-            YAML::Node entry{ YAML::NodeType::Map };
+            YAML::Node entry{YAML::NodeType::Map};
 
             entry["copy-to-deps"]["from"] = (path / "bin").u8string();
             entry["copy-to-deps"]["to"] = ".";
@@ -166,12 +140,12 @@ namespace re
             config["actions"].push_back(entry);
         }
 
-        auto package_target = std::make_unique<Target>(path.u8string(), "vcpkg." + cache_path, TargetType::StaticLibrary, config);
+        auto package_target =
+            std::make_unique<Target>(path.u8string(), "vcpkg." + cache_path, TargetType::StaticLibrary, config);
 
         YAML::Node vcpkg_json = YAML::LoadFile((vcpkg_root / "ports" / dep.name / "vcpkg.json").u8string());
 
-        auto append_deps_from = [this, &dep, &target, &package_target, &re_platform, cache](auto json)
-        {
+        auto append_deps_from = [this, &dep, &target, &package_target, &re_platform, cache](auto json) {
             if (auto deps = json["dependencies"])
             {
                 for (auto vcdep : deps)
@@ -204,7 +178,7 @@ namespace re
                             {
                                 if (re_platform == str.substr(1))
                                     eligible = false;
-                            }                   
+                            }
                             else if (re_platform != str)
                             {
                                 eligible = false;
@@ -212,7 +186,8 @@ namespace re
 
                             if (!eligible)
                             {
-                                // fmt::print(" ! EVIL dep {} has wrong platform '{}'\n", pkg_dep.name, platform.Scalar());
+                                // fmt::print(" ! EVIL dep {} has wrong platform '{}'\n", pkg_dep.name,
+                                // platform.Scalar());
                                 continue;
                             }
                         }
@@ -231,7 +206,7 @@ namespace re
                         continue;
                     }
 
-                    pkg_dep.resolved = { ResolveTargetDependency(target, pkg_dep, cache) };
+                    pkg_dep.resolved = {ResolveTargetDependency(target, pkg_dep, cache)};
 
                     package_target->dependencies.emplace_back(std::move(pkg_dep));
                 }
@@ -255,8 +230,8 @@ namespace re
         package_target->config["platform"] = re_platform;
         package_target->config["configuration"] = re_config;
 
-		if (dep.extra_config)
-			MergeYamlNode(package_target->config, dep.extra_config);
+        if (dep.extra_config)
+            MergeYamlNode(package_target->config, dep.extra_config);
 
         package_target->resolved_config = package_target->config;
 
@@ -280,14 +255,14 @@ namespace re
         of << emitter.c_str();
         */
 
-        auto& result = (mTargetCache[cache_path] = std::move(package_target));
+        auto &result = (mTargetCache[cache_path] = std::move(package_target));
         return result.get();
-	}
-    
-	bool VcpkgDepResolver::SaveDependencyToPath(const TargetDependency& dep, const fs::path& path)
+    }
+
+    bool VcpkgDepResolver::SaveDependencyToPath(const TargetDependency &dep, const fs::path &path)
     {
         YAML::Node config;
-        
+
         config["type"] = "project";
         config["name"] = dep.name;
 
@@ -304,4 +279,4 @@ namespace re
 
         return true;
     }
-}
+} // namespace re
