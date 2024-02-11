49174fb821041741cd5abcfe300022f8732660ac
osdever <10074604+osdeverr@users.noreply.github.com>
2024-02-11 21:51:09 +0300

    [vscode-launch] Fix error when .vscode was missing

--- re-vscode-launch.py
+++ re-vscode-launch.py
@@ -4,30 +4,37 @@ import os
 workspaceFolder = os.getcwd()
 
 meta = None
-with open('.re-cache/meta/full.json', 'r') as outfile:
+with open(".re-cache/meta/full.json", "r") as outfile:
     meta = json.load(outfile)
 if meta == None:
-    print('Required call re meta before running generation launch.json')
+    print("Required call re meta before running generation launch.json")
 
 current_launch = []
 try:
-    with open('.vscode/launch.json', 'r') as outfile:
+    with open(".vscode/launch.json", "r") as outfile:
         try:
             data = json.load(outfile)
-            current_launch = data['configurations']
+            current_launch = data["configurations"]
         except Exception as ex:
-            print('Parse launch.json error:', ex)
+            print("Parse launch.json error:", ex)
             exit(1)
 except Exception as ex:
     pass
 
+
 def read_meta_data():
     result = []
-    for index, (key, value) in enumerate(meta['targets'].items()):
+    for index, (key, value) in enumerate(meta["targets"].items()):
         try:
-            if value['type'] == 'executable':
-                result.append({'main-artifact': value['main-artifact'], 'module': value['module'],
-                              'arch': value['cxx']['arch'], 'config': value['cxx']['config']})
+            if value["type"] == "executable":
+                result.append(
+                    {
+                        "main-artifact": value["main-artifact"],
+                        "module": value["module"],
+                        "arch": value["cxx"]["arch"],
+                        "config": value["cxx"]["config"],
+                    }
+                )
         except:
             pass
     return result
@@ -50,12 +57,13 @@ def create_launch_cfg_item(main_artifact, module, arch, config):
         "cwd": "${fileDirname}",
         "environment": [],
         "console": "externalTerminal",
-        "re-defined": True
+        "re-defined": True,
         # "re-latest-meta": True,
         # "re-arch": f'{arch}',
         # "re-config": f'{config}',
         # "re-module": f'{module}'
     }
+
 
 meta_data = read_meta_data()
 
@@ -67,22 +75,23 @@ configurations = []
 #         configurations.append(launch_config)
 
 for launch_config in current_launch:
-    if 're-defined' in launch_config:
+    if "re-defined" in launch_config:
         current_launch.remove(launch_config)
 
 # print("current: ", current_launch)
 
 for module_info in meta_data:
-    main_artifact = module_info['main-artifact']
-    module = module_info['module']
-    arch = module_info['arch']
-    config = module_info['config']
+    main_artifact = module_info["main-artifact"]
+    module = module_info["module"]
+    arch = module_info["arch"]
+    config = module_info["config"]
 
     found = False
     for launch_config in current_launch:
-        if launch_config['name'] == generate_launch_name(main_artifact, module, arch, config):
-            launch_config = create_launch_cfg_item(
-                main_artifact, module, arch, config)
+        if launch_config["name"] == generate_launch_name(
+            main_artifact, module, arch, config
+        ):
+            launch_config = create_launch_cfg_item(main_artifact, module, arch, config)
             found = True
             break
 
@@ -97,9 +106,13 @@ for module_info in meta_data:
 
 configurations = current_launch + configurations
 
-data = {'version': '0.2.0', 'configurations': configurations}
+data = {"version": "0.2.0", "configurations": configurations}
 
-with open('.vscode/launch.json', 'w') as outfile:
-    outfile.write(json.dumps(data, indent=4))
+try:
+    os.makedirs(".vscode")
+    with open(".vscode/launch.json", "w") as outfile:
+        outfile.write(json.dumps(data, indent=4))
+except:
+    pass
 
 # print('.vscode/launch.json is ready')
