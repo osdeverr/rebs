033844f1495a344dcf60d5605d56798c505727b5
osdever <krasota156@gmail.com>
2024-02-15 14:43:13 +0300

    [vars/env] Use `ulib::env`

--- buildkit/re/build/environment_var_namespace.cpp
+++ buildkit/re/build/environment_var_namespace.cpp
@@ -1,11 +1,13 @@
 #include "environment_var_namespace.h"
 
+#include <ulib/env.h>
+
 namespace re
 {
     std::optional<std::string> EnvironmentVarNamespace::GetVar(const std::string &key) const
     {
-        if (auto var = std::getenv(key.c_str()))
-            return var;
+        if (auto var = ulib::getenv(ulib::u8(key)))
+            return ulib::sstr(*var);
         else
             return std::nullopt;
     }
